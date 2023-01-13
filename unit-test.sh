0ea7a62127174e1dbcc90cf386dfa8cea89e14b0
Arturo Molina <jamolina@gmail.com>
2023-01-12 23:31:04 -0600

    Reactivates virtual environment during unit test

--- unit-test.sh
+++ unit-test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "Making sure Python Virtual Environment is active"
+. .venv/bin/activate
+if [ $? -ne 0 ]; then
+    echo "**ERROR**: could not activate Python Virtual Environment." && exit 1
+fi
+
 # run unit tests
 echo "*** running unit tests ***"
 python -m coverage run --source=./src -m unittest discover -s unittest -p "test_*.py" -v
