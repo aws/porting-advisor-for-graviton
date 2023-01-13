9b70e3f468a6f34a2b06250fff3da57ac19a7964
Arturo Molina <jamolina@gmail.com>
2023-01-12 23:22:19 -0600

    Fixes coverage invocation on Unix/MacOS

--- unit-test.sh
+++ unit-test.sh
@@ -2,7 +2,7 @@
 
 # run unit tests
 echo "*** running unit tests ***"
-coverage run --source=./src -m unittest discover -s unittest -p "test_*.py" -v
+python -m coverage run --source=./src -m unittest discover -s unittest -p "test_*.py" -v
 if [ $? -ne 0 ]; then
     echo "**ERROR**: unit tests failed" && exit 1
 fi
