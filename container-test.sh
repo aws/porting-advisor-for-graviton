71286372a9a26768121220669ede0dd4f77cd7fc
Anushka Srinivasa <as14692@nyu.edu>
2024-05-09 09:53:01 -0700

    fix: Renaming test project manifest files to avoid (#54)
    security scanning
    
    Why: To prevent deprecated/vulnerable library dependencies in
    sample projects from being flagged during security scanning,
    as these sample projects are not intended for production use
    but needed to run integration tests.
    
    How: Renaming all manifest files
    (e.g., pom.xml, package.json, requirements.txt, Gemfile)
    to a placeholder name (_PLACEHOLDER) and renaming them back with
    the correct name during the integration test setup in a temp directory.

--- container-test.sh
+++ container-test.sh
@@ -5,13 +5,16 @@ source ./test-helpers.sh
 # This script tests container image build and runs a couple of
 # tests against the generated image.
 
+echo "Setting up sample-projects"
+sample_project_directory=$(get_sample_projects_relative_path)
+
 docker build -t porting-advisor .
 if [ $? -ne 0 ]; then
     echo "**ERROR**: building container image" && exit 1
 fi
 
 echo "Running container on samples to console"
-docker run --rm -v $(pwd)/sample-projects:/source porting-advisor /source > console_test.txt
+docker run --rm -v $(pwd)/$sample_project_directory:/source porting-advisor /source > console_test.txt
 test_report 'console' 'console_test.txt' "${lines_to_find[@]}"
 if [ $? -ne 0 ]; then
     echo "**ERROR**: running container to console" && exit 1
@@ -19,7 +22,7 @@ fi
 rm console_test.txt
 
 echo "Running container on samples to HTML report"
-docker run --rm -v $(pwd):/source porting-advisor /source/sample-projects --output /source/test.html 
+docker run --rm -v $(pwd):/source porting-advisor /source/$sample_project_directory --output /source/test.html 
 test_report 'html' 'test.html' "${lines_to_find[@]}"
 if [ $? -ne 0 ]; then
     echo "**ERROR**: running container to html report" && exit 1
